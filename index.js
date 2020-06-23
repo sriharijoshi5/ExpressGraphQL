91d857c0a14fe676011a7c1fb528dd174d703a87
sriharijoshi5 <sriharijoshi5@gmail.com>
2020-06-23 07:09:37 +0530

    Responding with Json values

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world")
+import Express from "express";
+import Products from "products.json"
+
+const app = Express();
+const port = 3000;
+
+
+// GET, PUT, POST, DELETE
+
+app.get("/", (req, res) => {
+    // res.send("Hello World");
+    res.json(Products);
+
+})
+
+app.listen(port, () => console.log("listening on port" + port))
+
+// app.put()
+// app.post()
+// app.delete()
+
+ 
